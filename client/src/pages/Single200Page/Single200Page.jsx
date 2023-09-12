f92ca03c3771b9364bfe63cbba00b3c30cb74dda
Marvinkwame <marvinkudjo@gmail.com>
2023-09-12 12:56:51 +0000

    Data changes

--- client/src/pages/Single200Page/Single200Page.jsx
+++ client/src/pages/Single200Page/Single200Page.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { BsArrowCounterclockwise } from "react-icons/bs"
+import { BsArrowCounterclockwise } from "react-icons/bs";
 
 const Single200Page = () => {
   const data = {
     1: {
       video: "https://www.youtube.com/embed/4FIJYu_dwFk?si=rR9n0hKFYr0ZzOE_",
       description: "abcd",
+      title: "abcd",
       subvideos: [
         "https://www.youtube.com/embed/OTUXWbSXWwI?si=ezxWIhahGAwfECKt",
-        "https://www.youtube.com/embed/OTUXWbSXWwI?si=ezxWIhahGAwfECKt"
+        "https://www.youtube.com/embed/OTUXWbSXWwI?si=ezxWIhahGAwfECKt",
       ],
     },
     2: {
-        video: "https://www.youtube.com/embed/4FIJYu_dwFk?si=rR9n0hKFYr0ZzOE_",
+      video: "https://www.youtube.com/embed/4FIJYu_dwFk?si=rR9n0hKFYr0ZzOE_",
       description: "abcd",
+      title: "abcd",
       subvideos: [
         "https://www.youtube.com/embed/OTUXWbSXWwI?si=ezxWIhahGAwfECKt",
-        "https://www.youtube.com/embed/OTUXWbSXWwI?si=ezxWIhahGAwfECKt"
+        "https://www.youtube.com/embed/OTUXWbSXWwI?si=ezxWIhahGAwfECKt",
       ],
     },
     3: {
@@ -39,16 +41,18 @@ const Single200Page = () => {
   return (
     <div className="py-20">
       <div className="max-w-[1444px] w-full my-0 mx-auto py-8 md:px-10 px-3">
-      <Link
+        <Link
           to="/all-courses/level100"
           className="font-semibold flex items-center gap-4 text-3xl text-primary hover:underline"
         >
-            <BsArrowCounterclockwise />
+          <BsArrowCounterclockwise />
           Back to Level 200 courses
         </Link>
         <div className="flex flex-col items-center justify-between">
           <div className="w-full mb-6">
-            <h2 className="text-center font-semibold text-3xl">{pageData.description}</h2>
+            <h2 className="text-center font-semibold text-3xl">
+              {pageData.title}
+            </h2>
             <iframe
               className="w-full aspect-video"
               src={pageData.video}
@@ -61,35 +65,27 @@ const Single200Page = () => {
 
           <div className="flex items-center w-full justify-between">
             <div className="w-[45%]">
-                <h2>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat
-                laboriosam dolore dignissimos placeat! Provident, autem aliquid
-                iste pariatur reiciendis aut, voluptatem maxime esse debitis
-                exercitationem tempora. Dicta autem quos, ad vel temporibus
-                optio quae obcaecati.
-                </h2>
+              <h2>{pageData.description}</h2>
             </div>
 
             <div className="p-6">
-            <h2>Critical Thinking</h2>
-            <div className="flex flex-col gap-y-4">
-              {pageData.subvideos.map((vid) => {
-                return (
-                  <iframe
-                  className="w-full aspect-auto"
-                    src={pageData.subvideos}
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen
-                  ></iframe>
-                );
-              })}
+              <h2>Critical Thinking</h2>
+              <div className="flex flex-col gap-y-4">
+                {pageData.subvideos.map((vid) => {
+                  return (
+                    <iframe
+                      className="w-full aspect-auto"
+                      src={vid}
+                      title="YouTube video player"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowfullscreen
+                    ></iframe>
+                  );
+                })}
+              </div>
             </div>
           </div>
-          </div>
-
-
         </div>
       </div>
     </div>
